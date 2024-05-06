e9b431fdd85442608138237566203e82d3cad803
Mattox <96073465+Mattox0@users.noreply.github.com>
2024-05-06 13:29:05 +0100

    feat(header): #MED-236 add header (#48)
    * feat(search): #MED-236 add search bar
    
    * feat(header): #MED-236 add responsive header

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,22 +1,52 @@
 import React from "react";
 
-import { Breadcrumb } from "@edifice-ui/react";
+import { Breadcrumb, SearchBar, Button } from "@edifice-ui/react";
+import { redirect } from "react-router-dom";
 import "./Header.scss";
 
 export const Header: React.FC = () => {
+  const [searchValue, setSearchValue] = React.useState("");
+
+  const search = () => {
+    console.log(searchValue);
+    return redirect("/search");
+    // redirect to search page
+  };
+
   return (
     <div className="med-header">
-      <Breadcrumb
-        app={{
-          address: "/mediacentre",
-          display: false,
-          displayName: "Médiacentre",
-          icon: "mediacentre",
-          isExternal: false,
-          name: "",
-          scope: [],
-        }}
-      />
+      <div className="med-header-container">
+        <Breadcrumb
+          app={{
+            address: "/mediacentre",
+            display: false,
+            displayName: "Médiacentre",
+            icon: "mediacentre",
+            isExternal: false,
+            name: "",
+            scope: [],
+          }}
+        />
+      </div>
+      <div className="med-header-container">
+        <SearchBar
+          isVariant={false}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setSearchValue(e.target.value)
+          }
+          onClick={() => search()}
+          placeholder="Rechercher une ressource"
+          size="md"
+        />
+        <Button
+          color="secondary"
+          type="button"
+          variant="outline"
+          className="med-header-button"
+        >
+          Recherche avancée
+        </Button>
+      </div>
     </div>
   );
 };
